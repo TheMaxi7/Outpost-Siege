c784061455ec68331672f72e6cf40ed3ab4e3dcf
Davide Altamura <davide.altamura97@gmail.com>
2024-04-01 18:15:13 +0200

    Added camera controller

---
+++ Assets/CameraController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed =30f;
+    public float scrollSpeed = 2000f;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(-Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(-Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        Vector3 pos = transform.position;
+
+        pos.y -= scroll * scrollSpeed * Time.deltaTime;
+
+        transform.position = pos;
+    }
+}
