959835af258666b67f6ffbf7a316d185d251d98a
Davide Altamura <davide.altamura97@gmail.com>
2024-05-02 18:17:22 +0200

    Polished camera movement
    - Lerp adjustments using curves;
    - Added clamping in all axis

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,33 +4,112 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float panSpeed =30f;
-    public float scrollSpeed = 2000f;
-    void Update()
+    public float movementSpeed;
+    public float movementTime;
+    public float rotationAmount;
+    public Transform cameraTransform;
+    public Vector3 zoomAmount;
+
+    public Vector3 newPosition;
+    public Quaternion newRotation;
+    public Vector3 newZoom;
+    public float zoomSpeed = 800f;
+
+    public Vector3 dragStartPosition;
+    public Vector3 dragCurrentPosition;
+    public Vector3 rotateStartPosition;
+    public Vector3 rotateCurrentPosition;
+
+    public AnimationCurve lerpCurve;
+
+    public float minX, minZ, minY;
+    public float maxX, maxZ, maxY;
+
+
+    private void Start()
     {
-        if (Input.GetKey(KeyCode.W))
+        newPosition = transform.position;
+        newRotation = transform.rotation;
+        newZoom = cameraTransform.localPosition;
+    }
+
+    private void Update()
+    {
+        HandleMovementInput();
+        HandleMouseInput();
+
+        float clampedY = Mathf.Clamp(newZoom.y, minY, maxY);
+        float clampedX = Mathf.Clamp(newPosition.x, minX, maxX);
+        float clampedZ = Mathf.Clamp(newPosition.z, minZ, maxZ);
+        newPosition = new Vector3(clampedX, clampedY, clampedZ);
+        newZoom.y = clampedY;
+
+    }
+
+    void HandleMovementInput()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(-Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+            newPosition += (transform.right * -movementSpeed);
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(-Vector3.left * panSpeed * Time.deltaTime, Space.World);
+            newPosition += (transform.right * movementSpeed);
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(-Vector3.back * panSpeed * Time.deltaTime, Space.World);
+            newPosition += (transform.forward * -movementSpeed);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(-Vector3.right * panSpeed * Time.deltaTime, Space.World);
+            newPosition += (transform.forward * movementSpeed);
         }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * rotationAmount);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, newPosition, lerpCurve.Evaluate(Time.deltaTime * movementSpeed));
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, lerpCurve.Evaluate(Time.deltaTime * rotationAmount));
+        
+    }
+
+    void HandleMouseInput()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+            if(plane.Raycast(ray, out entry))
+            {
+                dragStartPosition = ray.GetPoint(entry);
+            }
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+            if (plane.Raycast(ray, out entry))
+            {
+                dragCurrentPosition = ray.GetPoint(entry);
+                newPosition = transform.position + dragStartPosition - dragCurrentPosition;
+            }
+        }
+
 
         float scroll = Input.GetAxis("Mouse ScrollWheel");
+        newZoom.y -= scroll * zoomSpeed * Time.deltaTime;
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, lerpCurve.Evaluate(Time.deltaTime * movementSpeed));
 
-        Vector3 pos = transform.position;
 
-        pos.y -= scroll * scrollSpeed * Time.deltaTime;
-
-        transform.position = pos;
     }
 }
