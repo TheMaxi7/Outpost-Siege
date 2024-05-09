32e2282235440464d8d36bc6325f9339bf419267
Davide Altamura <davide.altamura97@gmail.com>
2024-05-09 16:42:48 +0200

    Added Game speed multiplier and Wave pause logic

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -10,30 +10,52 @@ public class WaveManager : MonoBehaviour
     public static bool spawn = false;
     private float timeBetweenWaves = 15f;
     private float timer;
+    private bool isSpawnPaused = false;
+    private float gameSpeedMultiplier = 1f;
 
     private void Start()
     {
         timer = 5f;
-
+        Time.timeScale = 1f;
     }
     private void Update()
     {
-        if (timer <= 0)
+        Time.timeScale = 1f * gameSpeedMultiplier;
+        if (!isSpawnPaused)
         {
-            waveNumber++;
-            spawn = true;
-            timer = timeBetweenWaves;
-            
+            if (timer <= 0)
+            {
+                waveNumber++;
+                spawn = true;
+                timer = timeBetweenWaves;
+            }
+            else
+            {
+                spawn = false;
+            }
         }
-        else
-        {
-            spawn = false;
-            
-        }
-
         timer -= Time.deltaTime;
 
     }
+
+    public void ToggleSpawn()
+    {
+        if (isSpawnPaused)
+            isSpawnPaused = false;
+        else
+            isSpawnPaused = true;
+
+    }
+
+    public void ToggleMultiplier()
+    {
+        if (gameSpeedMultiplier >= 4)
+            gameSpeedMultiplier = 1;
+        else
+            gameSpeedMultiplier *= 2;
+    }
+
+
 
 
 }
